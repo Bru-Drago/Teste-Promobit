c030c1719dfeccd41672724039e037b8f7adbbdb
Bru-Drago <bruna_drago@hotmail.com>
2020-10-14 15:23:40 -0300

    Instalando Alamofire e AlamoFireImage

---
+++ Pods/Target Support Files/Pods-TestePromobit-TestePromobitUITests/Pods-TestePromobit-TestePromobitUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestePromobit_TestePromobitUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TestePromobit_TestePromobitUITests
+@end
---
+++ Pods/Target Support Files/Pods-TestePromobit/Pods-TestePromobit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestePromobit : NSObject
+@end
+@implementation PodsDummy_Pods_TestePromobit
+@end
