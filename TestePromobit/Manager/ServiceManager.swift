38672f530d9a7e32d114ba4fcc3d413fa58b5516
Bru-Drago <bruna_drago@hotmail.com>
2020-10-14 16:32:52 -0300

    Adicionando data model e service manager

---
+++ TestePromobit/Manager/ServiceManager.swift
@@ -0,0 +1,18 @@
+//
+//  ServiceManager.swift
+//  TestePromobit
+//
+//  Created by Bruna Fernanda Drago on 14/10/20.
+//
+
+import Foundation
+
+class ServiceManager {
+    
+    static let shared = ServiceManager()
+    private init() {}
+    let baseURL = "https://gist.githubusercontent.com/rnas/c925888cb8f52306c82fc0dfc9a3fd79/raw/fb98b80d2a7e8d9dc0c4369650f518fa68f1e407/contacts.json"
+    
+  
+    
+}
---
+++ TestePromobit/Model/ContactData.swift
@@ -0,0 +1,33 @@
+//
+//  ContactData.swift
+//  TestePromobit
+//
+//  Created by Bruna Fernanda Drago on 14/10/20.
+//
+
+import Foundation
+
+struct ContactData {
+    
+    var userId       : Int
+    var userName     : String
+    var userLastname : String
+    var userCompany  : String
+    var userEmail    : String
+    var userNovo     : Bool
+    var userPhoto    : String
+    
+    enum CodingKeys : String,CodingKey {
+        case userID       = "user_id"
+        case userName     = "user_name"
+        case userLastname = "user_lastname"
+        case userCompany  = "user_company"
+        case userEmail    = "user_email"
+        case userNovo     = "user_novo"
+        case userPhoto    = "user_photo"
+    }
+}
+
+    
+    
+   
