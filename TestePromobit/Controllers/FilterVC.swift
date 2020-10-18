d4356303d2a27eb242e234a52f939ab1659cbeee
Bru-Drago <bruna_drago@hotmail.com>
2020-10-18 11:17:12 -0300

    adicionando uma nova view controller para o filtro de dados

---
+++ TestePromobit/Controllers/FilterVC.swift
@@ -0,0 +1,29 @@
+//
+//  FilterVC.swift
+//  TestePromobit
+//
+//  Created by Bruna Fernanda Drago on 18/10/20.
+//
+
+import UIKit
+
+class FilterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
